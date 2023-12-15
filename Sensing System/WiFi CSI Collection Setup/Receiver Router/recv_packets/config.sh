6ee606a6344e2589a2f913cecb190b999e66f569
Monjur-Mahathir <80934192+Monjur-Mahathir@users.noreply.github.com>
2023-12-15 08:42:23 -0500

    Update config.sh

--- Sensing System/WiFi CSI Collection Setup/Receiver Router/recv_packets/config.sh
+++ Sensing System/WiFi CSI Collection Setup/Receiver Router/recv_packets/config.sh
@@ -10,13 +10,7 @@ wl -i eth6 chanspec 157/80
 wl -i eth6 monitor 1
 ifconfig eth6 up
 
-# generate packet
-NODEID=$(ifconfig br0 | grep inet | awk '{ print $2 }' | awk -F"." '{ print $4 }')
-./setpackets $NODEID
-dd if=/dev/zero bs=4 count=1 of=/tmp/4zeroes
-cat /tmp/4zeroes packetnode1x1.dat > packetnode1x1BP.dat
-
-CONFIG=$(./makecsiparams -e 1 -m ff:ff:00:12:34:56 -c 0xe29b -C 1 -N 1 -b 0x88)
+CONFIG=$(./makecsiparams -e 1 -m ff:ff:00:12:34:56 -c 0xe29b -C 0xf -N 0x1 -b 0x88)
 LEN=34
 ./nexutil -I eth6 -s500 -b -l${LEN} -v${CONFIG} 
 
@@ -31,7 +25,7 @@ wl -i eth6 txcore -s 1 -c 1 -o 1 -k 1
 ./nexutil -Ieth6 -s528 -i -v15 -l 2
 
 # now reset channel
-# wl -i eth6 chanspec 157/$BW
+wl -i eth6 chanspec 157/$BW
 
 # equivalent to restarthfc.sh
 wl -i eth6 shmem 0x1776 0x9000
